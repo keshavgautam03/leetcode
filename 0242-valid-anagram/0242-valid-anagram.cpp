e3d0125089ffa3d0e278c520b6f87b3549a742dd
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-03-12 15:37:07 +0530

    Time: 3 ms (54.97%), Space: 9.6 MB (90.87%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int counts[26]={0};
+        int countt[26]={0};
+        bool answer=false;
+        for(int i=0;i<s.length();i++){
+            counts[s[i]-'a']++;
+            countt[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(countt[i]==counts[i]){
+                answer=true;
+            }
+            else{
+                // answer=false;
+                // break;
+                return false;
+            }
+
+        }
+        return answer;
+        
+    }
+};
