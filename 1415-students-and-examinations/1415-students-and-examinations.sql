b753ca5292a02148bf82f8ade623280c8245d07c
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-04-03 14:22:41 +0530

    Time: 1159 ms (78.71%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select st.student_id,st.student_name,su.subject_name,
 coalesce(e.attended_exams,0) as attended_exams
-from Subjects su
-cross join Students st
+from Students st
+cross join Subjects su
 left join (SELECT student_id, subject_name,COUNT(*) AS attended_exams
     FROM Examinations
     GROUP BY student_id, subject_name
