245b572fa6ee12b216edbe210fd2e547abc93489
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-04-16 23:02:28 +0530

    Time: 4833 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -0,0 +1,14 @@
+select distinct s.product_id as product_id,
+s.year as first_year,
+s.quantity as quantity,
+s.price as price
+from Sales as s
+join (
+    select product_id,min(year) as first_year
+    from Sales
+    group by product_id
+) p
+on p.product_id=s.product_id
+where s.year = p.first_year
+
+
