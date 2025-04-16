8232f9bdc93bc8bd5279feadd9a659d557252460
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-04-16 19:56:35 +0530

    Time: 3 ms (32.34%), Space: 95.8 MB (32.07%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,82 @@
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int m = nums1.size();
+//         int n = nums2.size();
+
+        
+//         std::vector<int> merged(m + n);
+
+//         int i = 0, j = 0, k = 0;
+
+        
+//         while (i < m && j < n) {
+//             if (nums1[i] <= nums2[j]) {
+//                 merged[k++] = nums1[i++];
+//             } else {
+//                 merged[k++] = nums2[j++];
+//             }
+//         }
+
+      
+//         while (i < m) {
+//             merged[k++] = nums1[i++];
+//         }
+
+        
+//         while (j < n) {
+//             merged[k++] = nums2[j++];
+//         }
+
+  
+//         if ((m + n) % 2 == 1) {
+            
+//             return merged[(m + n) / 2];
+//         } else {
+            
+//             int mid1 = (m + n) / 2;
+//             int mid2 = mid1 - 1;
+//             return (merged[mid1] + merged[mid2]) / 2.0;
+//         }
+//     }
+// };
+
+// Brute Force:
+               // 1.Merge Both Array
+              // 2.Sort them
+             // 3.Find Median
+            // TIME COMPLEXITY: O(n)+O(nlogn)+O(n)
+            // SPACE COMPLEXITY: O(1)
+ 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        vector<int> v;
+        
+        for (auto num : nums1) 
+            v.push_back(num);
+        
+        for (auto num : nums2) 
+            v.push_back(num);
+        
+        
+        sort(v.begin(), v.end()); 
+        
+        
+        int n = v.size(); 
+        
+        if (n % 2 == 1) {
+            
+            return v[n / 2];
+        } else {
+            
+            int mid1 = n / 2 - 1;
+            int mid2 = n / 2;
+            return (v[mid1] + v[mid2]) / 2.0;
+        }
+    }
+};
+
+
+
