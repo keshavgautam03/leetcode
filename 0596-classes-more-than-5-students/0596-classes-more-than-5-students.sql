0d919d4209e9a33bbbb58be7a36570c9f28ee608
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-04-17 13:21:35 +0530

    Time: 753 ms (21.44%), Space: 0B (100%) - LeetHub

---
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -0,0 +1,22 @@
+-- # Write your MySQL query statement below
+-- select class from Courses as c
+-- right join (select class,
+-- from Courses 
+-- GROUP BY class
+-- having count(student)>=5
+
+-- ) rj
+-- on rj.class=c.class
+-- SELECT c.class
+-- FROM Courses AS c
+-- RIGHT JOIN (
+--     SELECT class
+--     FROM Courses
+--     GROUP BY class
+--     HAVING COUNT(student) >= 5
+-- ) AS rj
+-- ON rj.class = c.class;
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
