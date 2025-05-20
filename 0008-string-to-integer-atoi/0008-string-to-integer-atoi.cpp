e3804e4603375cc5418fc183a559fc55483b66a1
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-05-20 13:58:44 +0530

    Time: 0 ms (100%), Space: 10.5 MB (5.35%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,43 +1,51 @@
+// class Solution {
+// public:
+//     int myAtoi(string s) {
+//          int i = 0, sign = 1;
+//         long result = 0;
+//         while (i < s.size() && s[i] == ' ') i++;
+//         if (i < s.size() && (s[i] == '-' || s[i] == '+')) {
+//             sign = (s[i] == '-') ? -1 : 1;
+//             i++;
+//         }
+//         while(i<s.size() && isdigit(s[i])){
+//             result=10*result+(s[i]-'0');
+//             i++;
+//             if(result*sign>INT_MAX){
+//                     return INT_MAX;
+//             }
+//             if(result*sign<INT_MIN){
+//                     return INT_MIN;
+//             }
+//         }
+//         return result * sign;
+//     }
+// }
+
+
 class Solution {
 public:
-    int recursion(string& s,int i,int sign,long ans){
-        if(i>=s.size() || !isdigit(s[i]))return ans*sign;
-        ans=ans*10 + (s[i]-'0');
-        if(ans*sign>INT_MAX)return INT_MAX;
-        if(ans*sign<INT_MIN)return INT_MIN;
-        return recursion(s,i+1,sign,ans);
+    long atoi(string s,int i,int sign,long ans){
+        if (i >= s.size() || !isdigit(s[i])) {
+            return sign * ans;
+        }
+        ans = ans * 10 + (s[i] - '0');
+        if (sign * ans > INT_MAX) return INT_MAX;
+        if (sign * ans < INT_MIN) return INT_MIN;
+        return atoi(s,i+1,sign,ans);
     }
-    int myAtoi(string s) { 
-        // Using recursion
-             long ans=0;
-             int i=0;
-             int sign=1;
-             while(i<s.size() && s[i]==' '){
-                i++;
-             }
-             if(i<s.size() && (s[i]=='-' || s[i]=='+')){
-                sign=(s[i]=='-')?-1:1;
-                i++;
-             }
-             return recursion(s,i,sign,ans);
-        // long ans=0;
-        // int n=s.size();
-        // int i=0;
-        // while (i < n && s[i] == ' ') {
-        //     i++;
-        // }
-        // int sign=1;
-        // if(s[i]=='-' || s[i]=='+'){
-        //     sign=(s[i]=='-')? -1 : 1;
-        //     i++;
-        // }
-        // while (i < n && s[i] >= '0' && s[i] <= '9') {
-        //     ans = ans * 10 + (s[i] - '0');         
-        //     if (ans * sign > INT_MAX) return INT_MAX;
-        //     if (ans * sign < INT_MIN) return INT_MIN;
+    int myAtoi(string s) {
+         int i=0;
+         int sign=1;
+         long ans;
+         while (i < s.size() && s[i] == ' '){
+            i++;
+         }
 
-        //     i++;
-        // }
-        // return (ans * sign);      
+        if (i < s.size() && (s[i] == '-' || s[i] == '+')) {
+         sign = (s[i] == '-') ? -1 : 1;
+         i++;
+        }
+         return atoi(s,i,sign,0);
     }
 };
