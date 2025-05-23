248c99354be9e19b42cc3c52dab2f0480b4d0002
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-05-23 18:45:48 +0530

    Time: 4 ms (40.58%), Space: 9.7 MB (74.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,32 +1,32 @@
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-//         vector<int>freq(26,0);
-//         for(char it : s){
-//             freq[it-'a']++;
-//         }
-//         for(char it : t){
-//             freq[it-'a']--;
-//         }
-//         for(int it : freq){
-//             if(it!=0){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// };
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]){
+        vector<int>freq(26,0);
+        for(char it : s){
+            freq[it-'a']++;
+        }
+        for(char it : t){
+            freq[it-'a']--;
+        }
+        for(int it : freq){
+            if(it!=0){
                 return false;
             }
         }
         return true;
     }
 };
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.size()!=t.size())return false;
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]!=t[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
