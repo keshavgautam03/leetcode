d67d84dc7dda3dc390d7a6c6b58b7af3c4223a7a
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-05-26 11:41:46 +0530

    Time: 0 ms (100%), Space: 11.7 MB (12.51%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,23 +1,48 @@
+// Dutch National Flag Algorithm
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zeroes=0;
-        int ones=0;
-        int twos=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                zeroes++;
+        int high=nums.size()-1;
+        int mid=0,low=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
             }
-            else if(nums[i]==1){
-                ones++;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                twos++;
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
-        int i = 0;
-        while (zeroes--) nums[i++] = 0;
-        while (ones--) nums[i++] = 1;
-        while (twos--) nums[i++] = 2;
     }
 };
+
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int zeroes=0;
+//         int ones=0;
+//         int twos=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0){
+//                 zeroes++;
+//             }
+//             else if(nums[i]==1){
+//                 ones++;
+//             }
+//             else{
+//                 twos++;
+//             }
+//         }
+//         int i = 0;
+//         while (zeroes--) nums[i++] = 0;
+//         while (ones--) nums[i++] = 1;
+//         while (twos--) nums[i++] = 2;
+//     }
+// };
