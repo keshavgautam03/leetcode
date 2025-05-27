15a49e2f614b2137ddb81adaaff618d53d2c6352
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-05-27 15:29:09 +0530

    Time: 5 ms (27.53%), Space: 15.4 MB (32.87%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void helper(int index,string curr,vector<string>&ans,int n,string s){
+        if(index==n){
+            return ans.push_back(curr);
+        }
+        if(isdigit(s[index])){
+            helper(index + 1, curr + s[index], ans, n, s);
+        }
+        else{
+          helper(index+1,curr+(char)tolower(s[index]),ans,n,s);
+          helper(index+1,curr+(char)toupper(s[index]),ans,n,s);  
+        }
+        
+    }
+    vector<string> letterCasePermutation(string s) {
+        vector<string>ans;
+        if(s.empty())return ans;
+        int n=s.length();
+        helper(0,"",ans,n,s);
+        return ans;
+    }
+};
