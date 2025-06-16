e63f19e3e0e2593619408bb97111e15c420d64ac
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-06-17 03:33:03 +0530

    Time: 0 ms (100%), Space: 16.2 MB (17.07%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&vis,vector<vector<char>>&board,int row,int col,vector<int>delr,vector<int>delc,int m, int n){
+    void dfs(vector<vector<int>>&vis,vector<vector<char>>&board,int row,int col,int m, int n){
         vis[row][col]=1;
+        vector<int>delc={0,+1,0,-1};
+        vector<int>delr={-1,0,+1,0};
         for(int i=0;i<4;i++){
             int nrow=row+delr[i];
             int ncol=col+delc[i];
             if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] &&
              board[nrow][ncol]=='O'){
-                dfs(vis,board,nrow,ncol,delr,delc,m,n);
+                dfs(vis,board,nrow,ncol,m,n);
              }
         }
     }
     void solve(vector<vector<char>>& board) {
         int m=board.size();
         int n=board[0].size();
-        vector<int>delc={0,+1,0,-1};
-        vector<int>delr={-1,0,+1,0};
+        
         vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             if(!vis[i][0] && board[i][0]=='O'){
-                dfs(vis,board,i,0,delr,delc,m,n);
+                dfs(vis,board,i,0,m,n);
             }
             if(!vis[i][n-1] && board[i][n-1]=='O'){
-                dfs(vis,board,i,n-1,delr,delc,m,n);
+                dfs(vis,board,i,n-1,m,n);
             }
         }
         for(int i=0;i<n;i++){
             if(!vis[0][i] && board[0][i]=='O'){
-                dfs(vis,board,0,i,delr,delc,m,n);
+                dfs(vis,board,0,i,m,n);
             }
             if(!vis[m-1][i] && board[m-1][i]=='O'){
-                dfs(vis,board,m-1,i,delr,delc,m,n);
+                dfs(vis,board,m-1,i,m,n);
             }
         }
         for(int i=0;i<m;i++){
