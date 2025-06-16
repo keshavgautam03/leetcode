70899cff4379881ef46c0c08687c421497528014
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-06-16 11:39:05 +0530

    Time: 4 ms (97.67%), Space: 34.8 MB (78.7%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,35 +3,29 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>dist(m,vector<int>(n,0));
-        vector<vector<int>>vis(m,vector<int>(n,0));
+        vector<vector<int>>dist(m,vector<int>(n,-1));
+        queue<pair<int,int>>q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    vis[i][j]=1;
-                    q.push({{i,j},0});
+                    dist[i][j] = 0;
+                    q.push({i,j});
                 }
             }
         }
-        vector<int>delr={+1,0,-1,0};
+        vector<int>delr={-1,0,+1,0};
         vector<int>delc={0,+1,0,-1};
         while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int dis=q.front().second;
+            auto [row,col]=q.front();
             q.pop();
-            dist[row][col]=dis;
             for(int i=0;i<4;i++){
-                int nrow=delr[i]+row;
-                int ncol=delc[i]+col;
-                if(nrow<m && ncol<n && nrow>=0 && ncol>=0 && vis[nrow][ncol]==0){
-                    vis[nrow][ncol]=1;
-                    q.push({{nrow,ncol},dis+1});
+                int nrow=row+delr[i];
+                int ncol=col+delc[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && dist[nrow][ncol]==-1){
+                    dist[nrow][ncol]=dist[row][col]+1;
+                    q.push({nrow,ncol});
                 }
-                
             }
-            
         }
         return dist;
     }
