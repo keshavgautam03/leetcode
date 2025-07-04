9d43c9c58d59b8deb1b19132ac3deb7e88d91404
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-07-05 00:25:21 +0530

    Time: 7 ms (49.61%), Space: 20.1 MB (31.54%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) { 
         vector<vector<int>> adj(numCourses);
         for (auto it : prerequisites) {
             adj[it[1]].push_back(it[0]);
         }
-
         vector<int> incoming(numCourses, 0);
         for (int i = 0; i < numCourses; i++) {
             for (auto it : adj[i]) {
                 incoming[it]++;
             }
         }
-
         queue<int> q;
         for (int i = 0; i < numCourses; i++) {
             if (incoming[i] == 0) {
                 q.push(i);
             }
         }
-
         int count = 0;
         while (!q.empty()) {
             int node = q.front();  
@@ -32,7 +28,5 @@ public:
             }
         }
         return count == numCourses;
-
-
     }
 };
