06752e5e55f02b0534d841b3dde5763509c3b4cc
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-07-10 16:37:43 +0530

    Time: 26 ms (42.51%), Space: 26.1 MB (50.2%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>dist(m,vector<int>(n,1e9));
+        queue<pair<int,pair<int,int>>>q;
+        if (grid[0][0] != 0 || grid[m-1][n-1] != 0) return -1;
+        q.push({1, {0, 0}});
+        dist[0][0] = 1;
+        vector<int> delr={-1,-1,0,+1,+1,+1,0,-1};
+        vector<int> delc={0,+1,+1,+1,0,-1,-1,-1};
+        while(!q.empty()){
+            int dis=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+            if (row == m - 1 && col == n - 1) return dis;
+            q.pop();
+
+            for(int i=0;i<8;i++){
+                int nrow=row+delr[i];
+                int ncol=col+delc[i];
+                
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]==0
+                && dis+1<dist[nrow][ncol]){
+                    dist[nrow][ncol]=dis+1;
+                    if(nrow==m-1 && ncol==n-1)return dis+1;
+                    q.push({dis+1,{nrow,ncol}});
+                }
+            }
+        }
+        return -1;
+
+    }
+};
