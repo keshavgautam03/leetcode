9afe233f3e55be392df4b952e3f9fa369be83396
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-07-14 01:01:12 +0530

    Time: 0 ms (100%), Space: 10.1 MB (80.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -18,19 +18,42 @@
 // };
 
 //mamozization
-    int f(int index,vector<int>&dp,vector<int>& nums){
-        if(index<0)return 0;
-        if(index==0)return nums[index];
-        if(dp[index]!=-1)return dp[index];
-        int pick=f(index-2,dp,nums)+nums[index];
-        int notPick=0+f(index-1,dp,nums);
-        return dp[index]=max(pick,notPick);
-    }
+
+//     int f(int index,vector<int>&dp,vector<int>& nums){
+//         if(index<0)return 0;
+//         if(index==0)return nums[index];
+//         if(dp[index]!=-1)return dp[index];
+//         int pick=f(index-2,dp,nums)+nums[index];
+//         int notPick=0+f(index-1,dp,nums);
+//         return dp[index]=max(pick,notPick);
+//     }
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>dp(n,-1);
+//         return f(n-1,dp,nums);
+//     }
+// };
+
+//tabulation with space optimization
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return f(n-1,dp,nums);
+        int prev2=0;
+        int prev=nums[0];
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1){
+                take+=prev2;
+            }
+            int notTake=prev;
+            int curri=max(take,notTake);
+            prev2=prev;
+            prev=curri;
+            
+        }
+        return prev;
     }
 };
