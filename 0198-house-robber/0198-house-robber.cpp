8b020b2f991ca6f27fcda578b921a0298995b258
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-07-14 00:41:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.72%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int suml=0;
+//         int sumr=0;
+//         int i=0;
+//         for(int j=2*i;j<nums.size();j++){
+//             sumr+=nums[j];
+//             i++;
+//         }
+//         int p=0;
+//         for(int j=2*p+1;j<nums.size();j++){
+//             suml+=nums[j];
+//             p++;
+//         }
+//         return max(suml,sumr);
+//     }
+// };
+
+//mamozization
+    int f(int index,vector<int>&dp,vector<int>& nums){
+        if(index<0)return 0;
+        if(index==0)return nums[index];
+        if(dp[index]!=-1)return dp[index];
+        int pick=f(index-2,dp,nums)+nums[index];
+        int notPick=0+f(index-1,dp,nums);
+        return dp[index]=max(pick,notPick);
+    }
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return f(n-1,dp,nums);
+    }
+};
