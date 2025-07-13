08306ff5c8354a9fa922f86332d48832e16914fd
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-07-13 23:53:21 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,11 +4,18 @@ public:
         if(n==1 || n==0){
             return 1;
         }
-        vector<int>dp(n+1);
-        dp[0]=dp[1]=1; //only one way to reach index 1 and 0
+        // vector<int>dp(n+1);
+        // dp[0]=dp[1]=1; 
+        //only one way to reach index 1 and 0
+        int prev2=1;
+        int prev=1;
+        int curri=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            curri=prev+prev2;
+            prev2=prev;
+            prev=curri;
+            
         }
-        return dp[n];
+        return prev;
     }
 };
