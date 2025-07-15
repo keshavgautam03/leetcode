a04ed95a4b73fb810d3a1721381efafff3bf3aeb
keshavgautam03 <119146955+keshavgautam03@users.noreply.github.com>
2025-07-15 10:19:40 +0530

    Time: 5 ms (69.37%), Space: 128.3 MB (57.56%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,25 +1,48 @@
+//mycode
+
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) {
+//       vector<int>pos,neg;
+//       int n=nums.size();
+      
+//       for(int i=0;i<n;i++){
+//         if(nums[i]>0){
+//             pos.push_back(nums[i]);
+//         }
+//         else{
+//             neg.push_back(nums[i]);
+//         }
+//       }
+//       vector<int>ans;
+//       int count1=0,count2=0;
+//       while(count1<n/2){
+//         ans.push_back(pos[count1]);
+//         count1++;
+//         ans.push_back(neg[count2]);
+//         count2++;
+//       }  
+//       return ans;
+//     }
+// };
+
+
+//better solution
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-      vector<int>pos,neg;
-      int n=nums.size();
-      
-      for(int i=0;i<n;i++){
-        if(nums[i]>0){
-            pos.push_back(nums[i]);
-        }
-        else{
-            neg.push_back(nums[i]);
-        }
-      }
-      vector<int>ans;
-      int count1=0,count2=0;
-      while(count1<n/2){
-        ans.push_back(pos[count1]);
-        count1++;
-        ans.push_back(neg[count2]);
-        count2++;
-      }  
-      return ans;
+        int n=nums.size();
+        vector<int> ans(n,0);
+        int posIndex=0, negIndex=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                ans[negIndex]=nums[i];
+                negIndex+=2;
+            }else{
+                ans[posIndex]=nums[i];
+                posIndex+=2;
+            }
+        }return ans;
     }
 };
+
